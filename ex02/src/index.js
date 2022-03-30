ad5d80424e35ce957fc6477df4233c3048034b90
desmosedici54 <mirrberr@gmail.com>
2022-03-30 12:37:58 +0200

    Adding the ex02

---
+++ ex02/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+
+//I don't know what to write
+
+/*Once again I don't know what to write
+I don't know what to write*/
+        
+    }
+    
+    main();
+    
+    module.exports = main;
